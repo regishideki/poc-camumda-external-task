8449e1797b306915d6defd72fcd7207f488edcac
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2019-05-10 18:37:51 -0300

    change typhoeus to ruby standard - auth + fetchAndLock working

--- automatic-credit-analysis.rb
+++ automatic-credit-analysis.rb
@@ -1,64 +1,79 @@
 require 'json'
-require 'typhoeus'
-
-module Infra
-  class HttpRequest
-    def initialize(base_url:, http_requester: Typhoeus::Request, timeout: 3000)
-      @base_url = base_url
-      @http_requester = http_requester
-      @timeout = timeout
-    end
-
-    def post(path:, params: nil, body: "", headers: {})
-      handle_response do
-        @http_requester.new(
-          "#{@base_url}/#{path}",
-          method: :post,
-          params: params,
-          body: body,
-          cache_ttl: 500,
-          connecttimeout_ms: @timeout,
-          timeout_ms: @timeout,
-          headers: headers
-        )
-      end
-    end
-
-    private
-    def handle_response
-      request = yield
-
-      request.on_complete do |response|
-        return response if response.success?
-
-        fail StandardError
-      end
-
-      request.run
-    end
-  end
-end
+require 'uri'
+require 'net/http'
 
 class CamundaExternalTaskScript
   attr_reader :request
 
-  def initialize
-    # falta autenticar e mudar o host para apontar para staging :(
-    @request = Infra::HttpRequest.new(base_url: 'http://localhost:8080/engine-rest/external-task')
-  end
-
   def run
     loop do
-      response = fetch_and_lock(fetch_body(external_tasks_topics))
-      decoded_response = JSON.parse(response.response_body)
+      url = URI("https://auth-staging.creditas.com.br/api/internal_clients/tokens")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Post.new(url)
+      request["accept-version"] = 'v1'
+      request["content-type"] = 'application/json'
+      request["cache-control"] = 'no-cache'
+      request.body = JSON.dump(
+        "grant_type": "password",
+        "username": "core",
+        "password": ""
+      )
+
+      response = http.request(request)
+      token = JSON.parse(response.read_body)['access_token']
+
+      url = URI("https://camunda.journey.stg.creditas.io/engine-rest/external-task/fetchAndLock")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Post.new(url)
+      request["authorization"] = "Bearer #{token}"
+      request["content-type"] = 'application/json'
+      request["cache-control"] = 'no-cache'
+      request.body = JSON.dump(
+        "workerId": "my-fancy-id",
+        "maxTasks": 1,
+        "usePriority": true,
+        "topics": [
+          {
+            "topicName": "AutomaticCreditAnalysis",
+            "lockDuration": 10000
+          }
+        ]
+      )
+
+      response = http.request(request)
+      decoded_response = JSON.parse(response.read_body)
 
       if decoded_response.any?
         puts decoded_response
         decoded_task = decoded_response.first
 
-        perform_credit_analysis(decoded_task)
+        url = URI("https://camunda.journey.stg.creditas.io/engine-rest/external-task/#{decoded_task['id']}/complete")
 
-        puts response.response_code
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+
+        request = Net::HTTP::Post.new(url)
+        request["authorization"] = "Bearer #{token}"
+        request["content-type"] = 'application/json'
+        request["cache-control"] = 'no-cache'
+        request.body = JSON.dump(
+          "workerId": "my-fancy-id",
+          "variables": {
+            'analysisResult': {
+              'value': creditAnalysisResult(decoded_task['businessKey'].to_i)
+            }
+          }
+        )
+
+        response = http.request(request)
+        puts response.code
+        puts response.read_body
       else
         print '.'
       end
@@ -67,58 +82,12 @@ class CamundaExternalTaskScript
     end
   end
 
-  def perform_credit_analysis(decoded_task)
-    complete_task(decoded_task, {
-      "creditAnalysisResult": {"value": creditAnalysisResult }
-    })
-  end
-
-  def creditAnalysisResult
-    # por enquanto, está aleatório, mas podemos responder de acordo com businessKey para ficar mais determinístico
-    ['R', 'A', 'G'].sample
-  end
-
-  def complete_task(decoded_task, variables)
-    request.post(
-      path: "#{decoded_task["id"]}/complete",
-      body: JSON.dump(complete_body(variables)),
-      headers: {'Content-Type' => 'application/json'}
-    )
-  end
-
-  def fetch_and_lock(body)
-    request.post(
-      path: 'fetchAndLock',
-      body: JSON.dump(body),
-      headers: {'Content-Type' => 'application/json'}
-    )
-  end
-
-  def complete_body(variables)
+  def creditAnalysisResult(business_key)
     {
-      "workerId": "my-fancy-id",
-      "variables":
-      {
-        #"approvalTeam": {"value": 'formalization'},
-      }.merge(variables)
-    }
-  end
-
-  def fetch_body(topics)
-    {
-      "workerId": "my-fancy-id",
-      "maxTasks": 1,
-      "usePriority": true,
-      "topics": topics
-    }
-  end
-
-  def external_tasks_topics
-    [{
-      "topicName": 'AutomaticCreditAnalysis',
-      "lockDuration": 10000,
-      "variables": []
-    }]
+      0 => 'rejected',
+      1 => 'approved',
+      2 => 'grey'
+    }.fetch(business_key % 3)
   end
 end
 
